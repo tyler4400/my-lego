bd3d30b12c723f7f41a05786261f8ff9b575951a
tyler4400 <tyler4400@foxmail.com>
2025-12-03 18:11:58 +0800

    创建shared子项目

---
+++ packages/shared/src/utils.ts
@@ -0,0 +1,21 @@
+export function isObject(value: unknown) {
+  return typeof value === 'object' && value !== null
+}
+
+export function isFunction(value: unknown) {
+  return typeof value === 'function'
+}
+
+export function isString(value: unknown) {
+  return typeof value === 'string'
+}
+
+export function isNumber(value: unknown) {
+  return typeof value === 'number'
+}
+
+export function isArray(value: unknown) {
+  return Array.isArray(value)
+}
+
+export const NOOP = () => {}
